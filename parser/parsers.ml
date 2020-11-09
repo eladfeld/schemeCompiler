bc33febec13be86a41a4e74db32f79c8b4356f3b
elad feldman <eladfeld@post.bgu.ac.il>
2020-11-09 17:03:16 +0200

    symbol parser is done

--- parser/parsers.ml
+++ parser/parsers.ml
@@ -12,6 +12,7 @@ let float_of_char c =
     let num = (int_of_char c)-(int_of_char '0') in
       float_of_int num
 
+
 (*-----------------------------------------end-------------------------------------------------*)
 
 let digit = range '0' '9';;
@@ -31,7 +32,6 @@ let nt_Sign =
 
 let nt_Natural = 
     let digits = plus digit in
-
       pack digits (fun (ds) -> (int_of_string (list_to_string ds)));;
 
 let nt_Mantissa = 
@@ -45,7 +45,27 @@ let nt_Mantissa =
                         ds 
                         0.
                 );;
-                    
+ (**)
+let nt_IntegerAsInteger =
+  let integ = caten nt_Sign nt_Natural in
+    pack integ 
+    (
+      fun ((sign,num)) -> 
+        sign*num
+    );; 
+    
+(*   *)
+let nt_Fraction = 
+  let frac = caten nt_IntegerAsInteger (caten (char '/') nt_Natural) in
+              pack frac
+                   (fun (nomer,(_,denom)) ->
+                      let gd = gcd nomer denom in
+                      Fraction(nomer/gd , denom/gd)
+                   )
+                 
+             ;;
+
+(*  *)
 let nt_Integer = 
   let integ = caten nt_Sign nt_Natural in
   pack integ 
@@ -54,13 +74,7 @@ let nt_Integer =
      Fraction(sign*num,1)
   );;
 
-let nt_IntegerAsInteger =
-  let integ = caten nt_Sign nt_Natural in
-    pack integ 
-    (
-      fun ((sign,num)) -> 
-        sign*num
-    );;
+
 
 let nt_Float = 
   let flo = caten nt_IntegerAsInteger (caten (char '.') nt_Mantissa) in
@@ -71,18 +85,6 @@ let nt_Float =
         then Float((float_of_int ls) -. rs)
         else Float((float_of_int ls) +. rs)
     );;
-
-
-
-let nt_Fraction = 
-  let frac = caten nt_IntegerAsInteger (caten (char '/') nt_Natural) in
-              pack frac
-                   (fun (nomer,(_,denom)) ->
-                      let gd = gcd nomer denom in
-                      Fraction(nomer/gd , denom/gd)
-                   )
-                 
-             ;;
              
 let nt_Number = disj nt_Float  
                (disj nt_Fraction nt_Integer);;
@@ -94,7 +96,7 @@ let nt_UpperCaseLetter = range 'A' 'Z';;
 
 
 let nt_PunctuationMarks = disj_list 
-  char '!' :: 
+  (char '!' :: 
   char '$' ::
   char '^' ::
   char '-' ::
@@ -105,26 +107,46 @@ let nt_PunctuationMarks = disj_list
   char '>' ::
   char '?' ::
   char '/' ::
-  char ':' ::[]
+  char ':' ::[]);;
 
 let nt_Dot = (char '.');;
 
+
 let nt_List = caten (char '(') 
-             (caten (star sexpr) (char ')'));;
+             (caten (star nt_sexpr) (char ')'));;
 
 let nt_DottedList = caten (char '(') 
                    (caten (plus nt_Sexpr) 
                    (caten (char '.') 
                    (caten nt_Sexpr (char ')'))));;
 
-let nt_SymbolCharNotDot = disj nt_digit 
+let nt_SymbolCharNotDot = disj digit 
                          (disj nt_LowerCaseLetter 
                          (disj nt_UpperCaseLetter nt_PunctuationMarks));;
 
-let nt_SymbolChar = disj nt_SymbolCharNoDot nt_SymbolChar ;;
+let nt_SymbolChar = disj nt_SymbolCharNotDot nt_Dot ;;
 
-let nt_Symbol =  disj nt_SymbolCharNotDot 
-                (caten nt_SymbolChar (plus nt_SymbolChar));;
+let nt_Symbol = 
+  let sym = disj  
+      (pack (caten nt_SymbolChar (plus nt_SymbolChar)) (fun(a, b) -> a::b))
+      (pack nt_SymbolCharNotDot (fun(a)->a::[]))
+       in 
+    pack sym(
+      fun(a)->
+      Symbol(list_to_string (List.map lowercase_ascii a))
+    );;
+
+let nt_StringMetaChar = caten 
+  (char '\'') 
+  (disj_list (
+    char '\''::
+    char '"'::
+    char 't'::
+    char 'f'::
+    char 'n'::
+    char 'r'::[]
+    ));;
+
 
 let nt_StringChar = disj nt_StringLiteralChar nt_StringMetaChar;;
 
@@ -132,5 +154,27 @@ let nt_StringChar = disj nt_StringLiteralChar nt_StringMetaChar;;
 let nt_String = caten (char '"') 
                (caten ( (star nt_StringChar) (char '"')));;
 
-let nt_Boolean = disj (word_ci "#f") (word_ci "t")
-test_string nt_Natural "123";;
+let parse_True = 
+  let tru  = word_ci "#t" in
+   pack tru 
+     (fun(a) ->
+     Bool(true));;
+
+let parse_False = 
+  let tru  = word_ci "#f" in
+   pack tru 
+     (fun(a) ->
+     Bool(false));;
+
+let nt_Boolean = disj parse_False parse_True;;
+
+
+let nt_sexpr = disj_list 
+  (nt_Boolean::
+  ny_char::
+  
+
+
+
+
+
