ea928df250f701325d77db25c4d1cec4763b42d7
Michael-amar <amarmic@post.bgu.ac.il>
2020-11-27 16:47:50 +0200

    up to lambda expressions

--- tag-parser.ml
+++ tag-parser.ml
@@ -44,15 +44,81 @@ let rec expr_eq e1 e2 =
                        
 exception X_syntax_error;;
 
+let is_Var x = 
+  let reserved_word_list =
+    ["and"; "begin"; "cond"; "define"; "else";
+    "if"; "lambda"; "let"; "let*"; "letrec"; "or";
+    "quasiquote"; "quote"; "set!"; "pset!"; "unquote";
+    "unquote-splicing"] in
+    not (List.mem x reserved_word_list);;  
 
+let rec is_proper_list pr = 
+  match pr with
+  | Nil -> true
+  | Pair(x, Nil)-> true
+  | Pair(x, Pair(y, z)) -> is_proper_list (Pair(y, z))
+  | Pair (x,y) -> false;;
+
+
+  (* let rec pairs_to_list_map pr func=
+    match pr with
+    | Pair(x,Nil) -> (func x)::[]
+    | Pair(x, Pair(y,z)) -> (func x)::(pairs_to_list_map (Pair(y,z)) func)
+    | Pair(x, y) -> (func x)::[(func y)]
+    | Nil -> []
+    | _ -> raise X_no_match;;
+
+let pairs_to_list pr = 
+    pairs_to_list_map pr (fun (Symbol(x))->x);; *)
+
+let rec pairs_to_list pr=
+  match pr with
+  | Pair(Symbol(x),Nil) -> x::[]
+  | Pair(Symbol(x), Pair(y,z)) -> x::(pairs_to_list (Pair(y,z)) )
+  | Pair(Symbol(x), Symbol(y)) -> x::[y]
+  | Nil -> []
+  | _ -> raise X_no_match;;
+
+
+let rec seperate_list_last lst = 
+  match lst with
+  |x::y::[] -> x::[] , y
+  |x::y -> let a, b = seperate_list_last y in x::a, b
+
+                  
+  
+  
 let rec tag_parse exp = 
   match exp with
   | Number(x) -> Const(Sexpr(Number(x)))
+  | Bool(x) -> Const(Sexpr(Bool(x)))
+  | Char(x) -> Const(Sexpr(Char(x)))
+  | String(x) -> Const(Sexpr(String(x)))
+  (* | Pair(Symbol("set!"), Pair(var, Pair(val, Nil))) -> Set(tag_parse var, tag_parse val)
+  | Pair(Symbol("define"), Pair(var, Pair(val, Nil))) -> Define(tag_parse var, tag_parse val) *)
+  | Symbol(x) -> Var(x)
   | Pair(Symbol("quote"), Pair(x, Nil)) -> Const(Sexpr(x))
   | Pair(Symbol("if"), Pair(test, Pair(dit, Pair(dif, Nil)))) -> If(tag_parse test, tag_parse dit, tag_parse dif)
-  | Pair(Symbol("set!"), Pair(var, Pair(val, Nil))) -> Set(tag_parse var, tag_parse val)
-  | Pair(Symbol("define"), Pair(var, Pair(val, Nil))) -> Define(tag_parse var, tag_parse val)
-  | _ -> X_syntax_error;;
+  | Pair(Symbol("if"), Pair(test, Pair(dit,Nil))) -> If(tag_parse test, tag_parse dit, Const(Void))
+  | Pair(Symbol("lambda"), x) -> parse_lambda  x
+  (* | Pair(Symbol(x), y) -> Applic(tag_parse x, pairs_to_list  *)
+  | _ -> raise X_syntax_error
+
+  and parse_lambda x = 
+   match x with 
+   | Pair(args,body) -> if(is_proper_list args) then 
+                            LambdaSimple(pairs_to_list args,tag_parse body)
+                        else let args=pairs_to_list args in 
+                              let mandatory, optional = seperate_list_last args in
+                            LambdaOpt(mandatory, optional, tag_parse body)
+                        
+   
+
+
+
+  
+
+  
 
 
 
