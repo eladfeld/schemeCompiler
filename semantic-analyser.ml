ca7953eb535e4032b975f70eeafd14c8ce78690e
elad feldman <eladfeld@post.bgu.ac.il>
2020-12-07 20:03:45 +0200

    first and sec parts completed
    Co-authored-by: מיכאל אמר <amarmic@post.bgu.ac.il>

--- semantic-analyser.ml
+++ semantic-analyser.ml
@@ -52,6 +52,79 @@ let rec expr'_eq e1 e2 =
                        
 exception X_syntax_error;;
 
+
+let rec index_of_rec lst element indx =
+  match lst with
+  | [] -> -1
+  | car::cdr -> 
+      if (car = element) then indx
+      else index_of_rec cdr element (indx + 1);;
+
+let index_of lst element = 
+  index_of_rec lst element 0;;
+
+let rec major_minor_rec lst element env_num= 
+  match lst with
+  | [] -> -1, -1
+  | car::cdr -> let indx = index_of car element in
+    if(indx < 0) then major_minor_rec cdr element (env_num +1)
+    else env_num, indx;;
+
+let major_minor lst element = 
+  major_minor_rec lst element 0;;
+
+let make_var x env=
+  match env with
+  | params::env -> let index = index_of params x in
+                  if(index >= 0) then 
+                    VarParam(x, index)
+                  else 
+                    let major, minor = major_minor env x in
+                    if(major >= 0) 
+                      then VarBound(x, major, minor)
+                    else VarFree(x)
+  | [] -> VarFree(x);;
+
+let rec annotate_lexical e env =
+  match e with 
+  | Const(c) -> Const'(c)
+  | If(test,dit,dif) -> If'(annotate_lexical test env, annotate_lexical dit env, annotate_lexical dif env)
+  | Seq(seq) -> Seq'(List.map (fun exp-> annotate_lexical exp env) seq)
+  | Set(Var(name),vl) -> Set'(make_var name env , annotate_lexical vl env)
+  | Def(Var(name),vl) -> Def'(make_var name env , annotate_lexical vl env)
+  | Or(ors) -> Or'(List.map (fun exp-> annotate_lexical exp env) ors) 
+  | Applic(func , arguments) -> Applic'(annotate_lexical func env, List.map (fun arg -> annotate_lexical arg env) arguments)
+  | LambdaSimple(params,body) -> LambdaSimple'(params , annotate_lexical body (params::env))
+  | LambdaOpt(mandatory , optional , body) -> LambdaOpt'(mandatory, optional , annotate_lexical body  ((List.append mandatory [optional])::env))
+  | Var(x) -> Var'(make_var x env)
+  | _ -> raise X_syntax_error;;
+
+let anottate x = 
+    annotate_lexical (List.hd ((Tag_Parser.tag_parse_expressions (read_sexprs x)))) [];;
+
+let rec annotate_TC expr in_tp = 
+  match expr with 
+  | Const'(x) -> Const'(x)
+  | Var'(var) -> Var'(var)
+  | If'(test,dit,dif) -> If'(annotate_TC test in_tp, annotate_TC dit in_tp, annotate_TC dif in_tp)
+  | Seq'(seq) -> Seq'(List.map (fun x -> annotate_TC x in_tp) seq)
+  | Set'(var,vl) -> Set'(var, annotate_TC vl in_tp)
+  | Def'(var,vl) ->Def'(var, annotate_TC vl in_tp)
+  | Or'(ors) -> Or'(List.map (fun x -> annotate_TC x in_tp) ors)
+  | LambdaSimple'(params, body) -> LambdaSimple'(params, annotate_TC body true)
+  | LambdaOpt'(mandatory, optional, body) -> LambdaOpt'(mandatory, optional, annotate_TC body true)
+  | Applic'(body, args) -> 
+    if(in_tp) then 
+      ApplicTP'(annotate_TC body false, List.map (fun arg -> annotate_TC arg false ) args)
+    else 
+      Applic'(annotate_TC body false, List.map (fun arg -> annotate_TC arg false ) args)
+  | _ -> raise X_no_match;;  
+  
+  
+  let tp_test x = 
+    annotate_TC (annotate_lexical (List.hd ((Tag_Parser.tag_parse_expressions (read_sexprs x)))) []);;
+
+    
 module type SEMANTICS = sig
   val run_semantics : expr -> expr'
   val annotate_lexical_addresses : expr -> expr'
@@ -59,9 +132,12 @@ module type SEMANTICS = sig
   val box_set : expr' -> expr'
 end;;
 
+
 module Semantics : SEMANTICS = struct
 
-let annotate_lexical_addresses e = raise X_not_yet_implemented;;
+let annotate_lexical_addresses e = 
+  annotate_lexical e [];;
+
 
 let annotate_tail_calls e = raise X_not_yet_implemented;;
 
