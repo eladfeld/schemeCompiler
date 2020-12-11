3b40658a32e60c3c2eb82b1a3a8f43cbec4fde86
elad feldman <eladfeld@post.bgu.ac.il>
2020-12-11 17:11:48 +0200

    some minor changes
    Co-authored-by: מיכאל אמר <amarmic@post.bgu.ac.il>

--- semantic-analyser.ml
+++ semantic-analyser.ml
@@ -124,66 +124,177 @@ let rec annotate_TC expr in_tp =
     | If'(test,dit,dif) ->  if_read test dit dif depth
     | LambdaSimple'(params,body) -> find_read body (depth + 1)
     | LambdaOpt'(mandatory, optional, body) -> find_read body (depth + 1)
-    | Or'(ors) -> List.fold_left (fun (acc, exp) -> (List.append (find_read exp depth) acc) ) [] ors
+    | Or'(ors) -> List.fold_left (fun acc exp -> (List.append (find_read exp depth) acc) ) [] ors
     | Set'(_,vl) -> find_read vl depth
-    | Seq'(seq) ->  List.fold_left (fun (acc, exp) -> List.append (find_read exp depth) acc ) [] seq
-    | Def'(_,vl) -> find_read vl
-    | Applic'(body,args) -> List.append (find_read body) (List.fold_left (fun (acc, exp) -> List.append (find_read exp depth) acc ) [] args)
-    | ApplicTP'(body,args) -> List.append (find_read body) (List.fold_left (fun (acc, exp) -> List.append (find_read exp depth) acc ) [] args)
+    | Seq'(seq) ->  List.fold_left (fun acc exp -> List.append (find_read exp depth) acc ) [] seq
+    | Def'(_,vl) -> find_read vl depth
+    | Applic'(body,args) -> List.append (find_read body depth) (List.fold_left (fun acc exp -> List.append (find_read exp depth) acc ) [] args)
+    | ApplicTP'(body,args) -> List.append (find_read body depth) (List.fold_left (fun acc exp -> List.append (find_read exp depth) acc ) [] args)
     | Var'(VarFree(name)) -> []
     | Var'(VarParam(name,minor)) -> if (depth == 0) then [name] else []
     | Var'(VarBound(name,major,minor)) -> if (depth - 1 == major) then [name] else []
     | _ -> []
 
 
-    (*
-      (lambda (x)
-      ->>
-        (list 
-          (x)
-          (lambda () (set! vb:0:0x 1))
-        )
-      )
-    *)
-    (* 
-    (lambda (x) 
-      (list 
-        (lambda() x lambda() x)
-        (lambda(y (set! x y)))))
-    
-    *)
     and if_read test dit dif depth = 
-      let test_read = find_read depth in
+      let test_read = find_read test depth in
       let dit_read = find_read dit depth in
       let dif_read = find_read dif depth in
-      test_read::dit_read::dif_read;;
+      List.append (List.append test_read dit_read) dif_read;;
 
 
+      let rec find_write exp depth = 
+        match exp with
+        | Const'(x) -> []
+        | If'(test,dit,dif) ->  if_write test dit dif depth
+        | LambdaSimple'(params,body) -> find_write body (depth + 1)
+        | LambdaOpt'(mandatory, optional, body) -> find_write body (depth + 1)
+        | Or'(ors) -> List.fold_left (fun acc exp -> (List.append (find_write exp depth) acc) ) [] ors
+        | Set'(vr,vl) -> List.append (write_var vr depth) (find_write vl depth)
+        | Seq'(seq) ->  List.fold_left (fun acc exp -> List.append (find_write exp depth) acc ) [] seq
+        | Def'(vr,vl) -> List.append (write_var vr depth) (find_write vl depth)
+        | Applic'(body,args) -> List.append (find_write body depth) (List.fold_left (fun acc exp -> List.append (find_write exp depth) acc ) [] args)
+        | ApplicTP'(body,args) -> List.append (find_write body depth) (List.fold_left (fun acc exp -> List.append (find_write exp depth) acc ) [] args)
+        | Var'(VarFree(name)) -> []
+        | Var'(VarParam(name,minor)) -> []
+        | Var'(VarBound(name,major,minor)) -> []
+        | _ -> []
+    
+        and write_var vr depth = 
+          match vr with
+          | VarParam(name,minor) -> if (depth == 0) then [name] else []
+          | VarBound(name,major,minor) -> if (depth - 1 == major) then [name] else []
+          | VarFree(name) -> []
 
-  let rec find_read_write  exp  =
-    let read = find_read exp 0 in 
-    (* let write = find_write exp 0  *)
-      read;;
+        and if_write test dit dif depth = 
+          let test_write = find_write test depth in
+          let dit_write = find_write dit depth in
+          let dif_write = find_write dif depth in
+          List.append (List.append test_write dit_write) dif_write;;
+
+  let find_read_test x =
+    find_read (annotate_lexical (List.hd ((Tag_Parser.tag_parse_expressions (read_sexprs x)))) []) (-1) ;;
+
+  let find_write_test x =
+    find_write (annotate_lexical (List.hd ((Tag_Parser.tag_parse_expressions (read_sexprs x)))) []) (-1) ;;
+
+  let rec exist element lst =
+    match lst with 
+    | hd::tail -> if (hd = element) then true else (exist element tail)
+    | [] -> false;; 
+    
+  let rec remove_dups lst = 
+      match lst with
+      | first::rest -> if (exist first rest) then (remove_dups rest) else first::(remove_dups rest)
+      | [] -> [];;
   
-  let apply_box expr =
-    let popo = find_read_write expr 0 in (Var'(VarFree("x")));;
+  let find_read_write exp  =
+    let read = remove_dups (find_read exp 0) in 
+    let write =remove_dups (find_write exp 0) in
+    (read, write);;
 
-
-  let rec check_box_set e =
+  let rec reach_lambda e =
     match e with
     | Const'(x) -> Const'(x)
     | Var'(var) -> Var'(var)
-    | If'(test,dit,dif) -> If'(check_box_set test,check_box_set dit, check_box_set dif)
-    | Seq'(seq) -> Seq'(List.map check_box_set seq)
-    | Set'(var,vl) -> Set'(check_box_set var, check_box_set vl)
-    | Def'(var,vl) -> Def'(check_box_set var, check_box_set vl)
-    | Or'(ors) -> Or'(List.map check_box_set ors)
+    | If'(test,dit,dif) -> If'(reach_lambda test,reach_lambda dit, reach_lambda dif)
+    | Seq'(seq) -> Seq'(List.map reach_lambda seq)
+    | Set'(var,vl) -> Set'(reach_lambda var, reach_lambda vl)
+    | Def'(var,vl) -> Def'(reach_lambda var, reach_lambda vl)
+    | Or'(ors) -> Or'(List.map reach_lambda ors)
     | LambdaSimple'(params, body) -> LambdaSimple'(params,apply_box body)
     | LambdaOpt'(mandatory, optional, body) ->  LambdaOpt'(mandatory,optional, apply_box body)
-    | Applic'(body, args) -> Applic'(check_box_set body, List.map check_box_set args)
-    | ApplicTP'(body,args) -> ApplicTP'(check_box_set body, List.map check_box_set args)
+    | Applic'(body, args) -> Applic'(reach_lambda body, List.map reach_lambda args)
+    | ApplicTP'(body,args) -> ApplicTP'(reach_lambda body, List.map reach_lambda args)
     | _ -> raise X_no_match;;  
+
+  let rec get_need_to_be_boxed_vars exp =
+    match exp with
+    | Const'(x) -> []
+    | Var'(var) ->  []
+    | If'(test,dit,dif) -> if_ribs_with_box test dit dif
+    | Seq'(seq) -> seq_ribs_with_box seq
+    | Set'(var,vl) -> []
+    | Def'(var,vl) -> []
+    | Or'(ors) -> []
+    | LambdaSimple'(params, body) ->  
+    | LambdaOpt'(mandatory, optional, body) -> 
+    | Applic'(body, args) -> 
+    | ApplicTP'(body,args) -> 
+    | _ -> raise X_no_match;;
     
+    (* 
+    (if x y 2)
+
+
+    (lambda (x) 
+      x
+      (lambda ()
+        (lambda (x)
+          (list 
+            x
+            (set! x 1)
+          )
+        )
+      )
+    )
+    *)
+    
+  let rec apply_box exp =
+    let need_to_be_boxed = get_need_to_be_boxed_vars exp in
+    match exp with
+    | Const'(x) -> 
+    | Var'(var) -> 
+    | If'(test,dit,dif) -> 
+    | Seq'(seq) -> 
+    | Set'(var,vl) -> 
+    | Def'(var,vl) -> 
+    | Or'(ors) -> 
+    | LambdaSimple'(params, body) -> 
+    | LambdaOpt'(mandatory, optional, body) ->  
+    | Applic'(body, args) -> 
+    | ApplicTP'(body,args) -> 
+    | _ -> raise X_no_match;;  
+
+
+[1:[r:x,y w:x,y]]
+
+[1:[r:y, w:x], 2:[r:x, w:y] ]
+    (* 
+    (lambda (x y z)
+      box(x)
+      box(y)
+      (list 
+      ->>
+        (lambda ()  
+          (set! x y)
+        ) 
+        (lambda ()
+          (set! y x))
+      )
+    *)
+    (* 
+    (lambda (x)  
+      3
+      (list 
+      ->>
+        (lambda (y)   
+          (set! x y))
+        (lambda () 
+          x 
+          lambda (x) 
+            x))
+    )
+    *)
+(* 
+    1 -> find read write  -> two arrays 
+    2 -> build a list of need-to-be-boxed vars -> list of string 
+    3 -> change tree to apply boxing -> tree
+*)
+
+
+
+
   let tp_test_string x = 
     annotate_TC (annotate_lexical (List.hd ((Tag_Parser.tag_parse_expressions (read_sexprs x)))) []) false;;
 
