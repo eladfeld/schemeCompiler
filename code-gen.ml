34a613f5722f2a026cb6baf6f0c713d5d2f12d1f
elad feldman <eladfeld@post.bgu.ac.il>
2020-12-25 18:28:51 +0200

    const_tbl and fvar_tbl ready
    Co-authored-by: מיכאל אמר <amarmic@post.bgu.ac.il>

--- code-gen.ml
+++ code-gen.ml
@@ -33,8 +33,8 @@ end;;
 
 let rec collect_sexp expr =
   match expr with
-  | Const'(Void) -> [Void]
-  | Const'(Sexpr(x)) -> [Sexpr(x)]
+  | Const'(Void) -> []
+  | Const'(Sexpr(x)) -> [x]
   | If'(test,dit,dif) -> List.append (List.append (collect_sexp test) (collect_sexp dit)) (collect_sexp dif)
   | LambdaSimple'(params,body) -> collect_sexp body
   | LambdaOpt'(mandatory, optional, body) -> collect_sexp body
@@ -60,12 +60,18 @@ let rec collect_sexp expr =
   let expand_sexpr_list sexpr_list = 
     let rec expand_sexpr expr = 
       match expr with
-      | Sexpr(Pair(car,cdr)) -> List.append (List.append (expand_sexpr car)(expand_sexpr cdr)) [Sexpr((Pair(car,cdr)))]
-      | Sexpr(Symbol(s)) -> Sexpr(String(s))::[Sexpr(Symbol(s))]
-      | Sexpr(sexp) -> [Sexpr(sexp)] in
+      | Pair(car,cdr) -> List.append (List.append (expand_sexpr car)(expand_sexpr cdr)) [Sexpr((Pair(car,cdr)))]
+      | Symbol(s) -> Sexpr(String(s))::[Sexpr(Symbol(s))]
+      | sexp -> [Sexpr(sexp)] in
     List.concat (List.map (fun sexpr -> expand_sexpr sexpr) sexpr_list);;
 
-  let find_sexpr_offset sexp const_tbl = "";;
+  let rec find_sexpr_offset sexp const_tbl = 
+    match const_tbl with 
+    | entry::rest -> let (cur_sexpr, (offset, _)) = entry in
+        let Sexpr(in_sexp) = sexp in
+        let Sexpr(cur_sexpr) = cur_sexpr in  
+        if (sexpr_eq in_sexp cur_sexpr) then (string_of_int offset) else find_sexpr_offset sexp rest
+    | [] -> "---------WARNING NO SUCH CONST DEFINED-------------";;
 
   let rec build_const_tbl sexprs const_tbl current_offset=
     let sexpr_to_const_entry sexpr const_tbl current_offset=
@@ -78,14 +84,37 @@ let rec collect_sexp expr =
       | Sexpr(Number(Float(flt))) -> (current_offset + 9 ,(Sexpr(Number(Float(flt))), (current_offset , "MAKE_LITERAL_FLOAT(" ^ (string_of_float flt) ^ ")\n")))
       | Sexpr(Char(c)) -> (current_offset + 2 ,(Sexpr(Char(c)), (current_offset, "MAKE_LITERAL_CHAR(" ^ (String.make 1 c) ^ ")\n")))
       | Sexpr(String(str)) -> (current_offset + 9 + (String.length str) ,(Sexpr(String(str)), (current_offset, "MAKE_LITERAL_STRING " ^ str ^"\n") ) )
-      | Sexpr(Symbol(symb)) -> (current_offset + 9 ,(Sexpr(Symbol(symb)) , (current_offset  , "MAKE_LITERAL_SYMBOL(const_tbl+" ^ (find_sexpr_offset symb const_tbl) ^ ")\n")))
-      | Sexpr(Pair(car,cdr)) -> (current_offset + 17, (Sexpr(Pair(car,cdr)) , (current_offset  , "MAKE_LITERAL_PAIR(const_tbl+" ^ (find_sexpr_offset car const_tbl) ^ ",const_tbl+" ^ (find_sexpr_offset cdr const_tbl) ^")\n")))
+      | Sexpr(Symbol(symb)) -> (current_offset + 9 ,(Sexpr(Symbol(symb)) , (current_offset  , "MAKE_LITERAL_SYMBOL(const_tbl+" ^ (find_sexpr_offset (Sexpr(String(symb))) const_tbl) ^ ")\n")))
+      | Sexpr(Pair(car,cdr)) -> (current_offset + 17, (Sexpr(Pair(car,cdr)) , (current_offset  , "MAKE_LITERAL_PAIR(const_tbl+" ^ (find_sexpr_offset (Sexpr(car)) const_tbl) ^ ",const_tbl+" ^ (find_sexpr_offset (Sexpr(cdr)) const_tbl) ^")\n")))
       in
     match sexprs with
     | sexpr::rest -> let (new_offset, entry) = (sexpr_to_const_entry sexpr const_tbl current_offset) in
                         build_const_tbl rest (List.append const_tbl [entry]) new_offset
     | [] -> const_tbl
 
+  let rec collect_fvars expr =
+    match expr with
+    | Const'(Void) -> []
+    | Const'(Sexpr(x)) -> [] 
+    | If'(test,dit,dif) -> List.append (List.append (collect_fvars test) (collect_fvars dit)) (collect_fvars dif)
+    | LambdaSimple'(params,body) -> collect_fvars body
+    | LambdaOpt'(mandatory, optional, body) -> collect_fvars body
+    | Or'(ors) -> List.fold_left (fun acc o -> List.append acc (collect_fvars o)) [] ors
+    | Set'(vr,vl) -> collect_fvars vl
+    | Seq'(seq) ->  List.fold_left (fun acc e -> List.append acc (collect_fvars e)) [] seq
+    | Def'(vr,vl) -> collect_fvars vl
+    | Applic'(body,args) -> List.append (collect_fvars body) (List.fold_left (fun acc arg -> List.append acc (collect_fvars arg)) [] args)
+    | ApplicTP'(body,args) -> List.append (collect_fvars body) (List.fold_left (fun acc arg -> List.append acc (collect_fvars arg)) [] args)
+    | Var'(VarFree(name)) -> [name]
+    | Var'(x) -> []
+    | BoxSet'(vr,vl) -> collect_fvars vl
+    | BoxGet'(vr) -> []
+    | Box'(var) -> [];;
+
+  let rec build_fvars_tbl fvars index= 
+      match fvars with
+      | first::rest -> (first, index)::(build_fvars_tbl rest (index+1))
+      | [] -> [];;
 
 module Code_Gen : CODE_GEN = struct
   let make_consts_tbl asts = 
@@ -97,7 +126,11 @@ module Code_Gen : CODE_GEN = struct
     build_const_tbl sorted_sexprs_set [] 0 ;;
     
     
-  let make_fvars_tbl asts = raise X_not_yet_implemented;;
+  let make_fvars_tbl asts = 
+    let fvars_list = List.fold_left (fun acc ast -> List.append acc (collect_fvars ast)) [] asts in
+    let fvars_set = remove_dups fvars_list [] in
+    build_fvars_tbl fvars_set 0;;
+    
   let generate consts fvars e = raise X_not_yet_implemented;;
 end;;
 
@@ -115,5 +148,12 @@ let test_make_const_table str=
   let sexprs_list = List.fold_left (fun acc ast -> List.append acc (collect_sexp ast)) [] (List.map (fun tag_parsed -> Semantics.run_semantics tag_parsed) (Tag_Parser.tag_parse_expressions (Reader.read_sexprs str))) in
   let sexprs_set = remove_dups sexprs_list [] in
   let sorted_sexprs_list = expand_sexpr_list sexprs_set  in
+  (* let sorted_sexprs_list = List.append [Void; Sexpr(Nil) ; Sexpr(Bool(true)) ; Sexpr(Bool(false))] sorted_sexprs_list in *)
   let sorted_sexprs_set = remove_dups sorted_sexprs_list [] in 
   build_const_tbl sorted_sexprs_set [] 0 ;;
+
+
+let test_make_fvars_table str = 
+  let fvars_list = List.fold_left (fun acc ast -> List.append acc (collect_fvars ast)) [] (List.map (fun tag_parsed -> Semantics.run_semantics tag_parsed) (Tag_Parser.tag_parse_expressions (Reader.read_sexprs str))) in
+  let fvars_set = remove_dups fvars_list [] in
+  build_fvars_tbl fvars_set 0;;
